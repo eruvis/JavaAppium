710078493a0d1e6dec7d6b51b14fe44b9803adbd
eruvis <eruvis@mail.ru>
2022-02-02 18:12:04 +0700

    add code from lesson 10.10-11

--- src/test/java/tests/ArticleTests.java
+++ src/test/java/tests/ArticleTests.java
@@ -1,7 +1,6 @@
 package tests;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
 import lib.CoreTestCase;
 import lib.ui.ArticlePageObject;
@@ -11,8 +10,10 @@ import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
+@Epic("Test for articles")
 public class ArticleTests extends CoreTestCase {
     @Test
+    @Features(value = {@Feature(value = "Search"), @Feature(value = "Article")})
     @DisplayName("Compare article title with expected one")
     @Description("We open 'Java Object-oriented programming language' article and make sure the title is expected")
     @Step("Starting test testCompareArticleTitle")
@@ -35,6 +36,7 @@ public class ArticleTests extends CoreTestCase {
     }
 
     @Test
+    @Features(value = {@Feature(value = "Search"), @Feature(value = "Article")})
     @DisplayName("Swipe article to the footer")
     @Description("We open an article and swipe it to the footer")
     @Step("Starting test testSwipeArticle")
